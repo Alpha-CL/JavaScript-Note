912bd96221ba665f89cceeb7cdc346e6d3d700de
Alpha-CL <xiaodao_92@yeah.net>
2020-05-14 20:45:59 +0800

    daily update

--- 7.1-Engineering/Case/case-2-commonjs-eg/test2.js
+++
@@ -1,5 +0,0 @@
-const Poker = require('./poker.js');
-
-const p = new Poker(1, 13);
-
-console.log(p);
--- 7.1-Engineering/Case/case-3-amd/a.js
+++
@@ -1,6 +0,0 @@
-define(function () {
-
-    console.log('a模块执行');
-
-    return 'hello world';
-});
