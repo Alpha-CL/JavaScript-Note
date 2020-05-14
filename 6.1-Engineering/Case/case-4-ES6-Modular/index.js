7ef732e285e64bba53494ae49e004668fcf40027
Alpha-CL <xiaodao_92@yeah.net>
2020-05-15 04:40:53 +0800

    daily update

--- 6.1-Engineering/Case/case-4-ES6-Modular/index.js
+++ 6.1-Engineering/Case/case-4-ES6-Modular/index.js
@@ -15,22 +15,53 @@
 // console.log(name, age, gender, sayHi());
 
 
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -//
+
+
+// /**
+//  * basic import object
+//  *
+//  * import * as obj from "exportUrl";
+//  */
+//
+// import * as res from "./a.js";
+//
+// console.log(res);
+//
+// console.log(res.name, res.age, res.gender);
+//
+// console.log(res.sayHi());
+
+
 //-------------------------------------------------------------------------------------------------------------------//
 
 
-/**
- * basic import object
- *
- * import * as obj from "exportUrl";
- */
+// /**
+//  * Default Import
+//  *
+//  *
+//  * 由于默认导入时变量名是自定义的，因此无别名一说
+//  */
+//
+// import user from "./a.js";
+//
+// console.log(user);
 
-import * as res from "./a.js";
 
-console.log(res);
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -//
 
-console.log(res.name, res.age, res.gender);
 
-console.log(res.sayHi());
+// /**
+//  * 同时导入某个模块的默认导出和基本导出
+//  *
+//  *
+//  */
+//
+// import user, {sayYes} from "./a.js";
+//
+// console.log(user);
+//
+// console.log(sayYes());
 
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
