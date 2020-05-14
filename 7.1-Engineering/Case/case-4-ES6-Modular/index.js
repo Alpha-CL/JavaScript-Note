3dfd0863d037b11e403c56cd9170b34717528b08
Alpha-CL <xiaodao_92@yeah.net>
2020-05-14 20:44:53 +0800

    daily update

---
+++ 7.1-Engineering/Case/case-4-ES6-Modular/index.js
@@ -0,0 +1,36 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+// /**
+//  * basic import of ( prop/method )
+//  *
+//  * import { exportList } from "exportUrl";
+//  */
+//
+// import {
+//     name as user, age, gender, sayHi
+//
+// } from "./a.js";
+//
+// console.log(name, age, gender, sayHi());
+
+
+//-------------------------------------------------------------------------------------------------------------------//
+
+
+/**
+ * basic import object
+ *
+ * import * as obj from "exportUrl";
+ */
+
+import * as res from "./a.js";
+
+console.log(res);
+
+console.log(res.name, res.age, res.gender);
+
+console.log(res.sayHi());
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
