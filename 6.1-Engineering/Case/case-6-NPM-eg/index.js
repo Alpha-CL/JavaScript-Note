a390cfd8db0cef1f8ecf3d5c8cff2a682c09732a
Alpha-CL <xiaodao_92@yeah.net>
2020-05-18 02:23:34 +0800

    daily update

--- 6.1-Engineering/Case/case-6-NPM-eg/index.js
+++ 6.1-Engineering/Case/case-6-NPM-eg/index.js
@@ -1,14 +1,15 @@
-// import {getMovieInfo} from "./getMovieInfo.js";
-//
-// console.log(getMovieInfo());
+let getMovieData = require('./getMovieInfo'),
+    fs = require('fs');
 
+getMovieData().then(movies => {
 
+    let json = JSON.stringify(movies);
 
+    fs.writeFile('movie.json', json, function () {
 
-
-
-
-
+        console.log('successful');
+    });
+});
 
 
 
