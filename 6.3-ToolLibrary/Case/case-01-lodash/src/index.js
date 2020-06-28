a4f7edd098b16aeba703522755c26ba33ec3f97d
Alpha-CL <xiaodao_92@yeah.net>
2020-06-28 18:48:13 +0800

    daily update

--- 6.3-ToolLibrary/Case/case-01-lodash/src/index.js
+++ 6.3-ToolLibrary/Case/case-01-lodash/src/index.js
@@ -2,3 +2,7 @@ import {moduleHot} from "./assets/serverhot.config.js";
 moduleHot();
 
 import "./assets/js/lodash-array";
+import "./assets/js/lodash-collection";
+import "./assets/js/lodash-lang";
+import "./assets/js/lodash-object";
+import "./assets/js/lodash-string";
